b221233e411c9b5aedc25a82bbf48955b4096d42
Rishabh Sukhwani <rishabhsukhwani3@gmail.com>
2023-05-14 13:52:01 +0530

    feat: added react-icons to meals component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Favourites from './components/Favourites'
 import Meals from './components/Meals'
 import Modal from './components/Modal'
 import Search from './components/Search'
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 function App() {
 
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AppContext, useGlobalContext } from "../context";
-
+import {BsHandThumbsUp} from 'react-icons/bs'
 
 const Meals = () => {
 
@@ -17,7 +17,7 @@ const Meals = () => {
                     <img src={image} className="img" />
                     <footer>
                         <h5>{title}</h5>
-                        <button className="like-btn">click me</button>
+                        <button className="like-btn">< BsHandThumbsUp /></button>
                     </footer>
                 </article>
             })}
